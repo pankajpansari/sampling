2ea68e905f4851120e13357d0fc7465ef3940c9b
pankajpansari <pankaj.pansari1@gmail.com>
2018-05-29 11:57:05 +0100

    Implemented basic version of frank-wolfe (without much optimisation)

--- frank_wolfe.py
+++ frank_wolfe.py
@@ -1,59 +1,72 @@
-def getCondGrad(grad):
-    n = grad.shape[0]
-    y = Variable(torch.zeros(n)) #conditional grad
-    for p in range(n):
-        if grad.data[p] > 0:
-            y[p] = 1
-        else:
-            y[p] = 0
-    return y
+import networkx as nx
+import sys
+import numpy as np
+import math
+import torch
+from influence import ic_model as submodObj
+from torch.autograd import Variable
+import logger
+from builtins import range
 
-def grad_importance(L, x, net, use_net, nsamples):
+def getRelax(G, x, nsamples): 
+
+    current_sum = Variable(torch.FloatTensor([0]), requires_grad = True) 
+
+    for trial in range(nsamples):
+        sample = Variable(torch.bernoulli(x.data))
+        current_sum = current_sum + submodObj(G, sample)
+
+    return current_sum/nsamples
+
+def getCondGrad(grad, k):
+    #conditional gradient for cardinality constraints
+    N = grad.shape[0]
+    top_k = Variable(torch.zeros(N)) #conditional grad
+    sorted_ind = torch.sort(grad, descending = True)[1][0:k]
+    top_k[sorted_ind] = 1
+    positive = grad > 0
+    top_k_positive = (top_k.byte() * positive).float()
+    return top_k_positive 
+
+def getGrad(G, x, nsamples):
 #Returns the gradient vector of the multilinear relaxation at x as given in Chekuri's paper
 #(See Theorem 1 in nips2012 paper)
-    N = L.shape[0]
+    N = G.number_of_nodes()
     grad = Variable(torch.zeros(N))
-    input_sample = torch.cat((x, L.view(1, N*N)), dim = 1)
-    if use_net == 1:
-#        z = net(input_sample, 0) 
-        z = net(x, 0) 
-    else:
-        z = x.clone()
 
-#    if use_net == 1:
-#        print x, z
     for p in np.arange(N):
-        z_include = z.clone()
-        z_exclude  = z.clone() 
-        z_include[:, p] = 1
-        z_exclude[:, p] = 0
-
         x_include = x.clone()
         x_exclude  = x.clone() 
-        x_include[:, p] = 1
-        x_exclude[:, p] = 0
+        x_include[p] = 1
+        x_exclude[p] = 0
 
-        grad[p] =  multilinear_importance(x_include, z_include, L, nsamples) - multilinear_importance(x_exclude, z_exclude, L, nsamples)
-
+        grad[p] =  getRelax(G, x_include, nsamples) - getRelax(G, x_exclude, nsamples)
     return grad
 
 
-def runFrankWolfe(graph_file, nsamples):
+def runFrankWolfe(G, nsamples):
 
-    N = L.shape[0]
-    x = Variable(torch.Tensor(1, N))
-    X = Variable(torch.Tensor(1, (N + 1)*N))
-    x[0] = Variable(torch.Tensor([0.5]*N))
+    N = nx.number_of_nodes(G)
 
+    x = Variable(torch.Tensor([0]*N))
+    
     for iter_num in np.arange(1, 50):
 
-        grad = getGrad(L, x, net, use_net, nsamples)
-        x_star = getCondGrad(grad)
+        grad = getGrad(G, x, nsamples)
+
+        x_star = getCondGrad(grad, 10)
 
         step = 2.0/(iter_num + 2) 
 
         x = step*x_star + (1 - step)*x
-    
-    currentVal = getExactRelax(L, x)
-    return x, currentVal.data[0]
 
+        obj = getRelax(G, x, nsamples)
+        
+        print "Iteration: ", iter_num, "    obj = ", obj, "     x = ", x
+   return x 
+
+def main():
+    grad = torch.randn(10)
+
+if __name__ == '__main__':
+    main()
