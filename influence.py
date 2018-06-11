070754b579d2bba2e2802c84f715270bb60f55fe
pankajpansari <pankaj.pansari1@gmail.com>
2018-06-11 16:34:16 +0100

    Added code for variance study of mc simulations of diffusion model.2

--- influence.py
+++ influence.py
@@ -7,6 +7,8 @@ import ndlib.models.ModelConfig as mc
 import time
 import random
 import torch
+import os
+import math
 from torch.autograd import Variable
 
 random.seed(1234)
@@ -58,12 +60,60 @@ def ic_model(G, sample, p, iterations):
 #    print 'Total influence:',int(round(avg_influence.item()))
     return avg_influence
 
-def main():
-    g = nx.erdos_renyi_graph(1000, 0.2)
-    tic = time.clock()
-    ic_model(g)
-    toc = time.clock()
-    print "Time elapsed = ", toc - tic
+def read_graph(filename):
+    f = open(filename, 'rU')
+    nNodes = 0
+    for line in f:
+        if line == '\n':
+            break
+        else:
+            nNodes += 1
+    
+    alphas = np.zeros((nNodes, nNodes))
+    
+    for line in f:
+        temp = line.strip('\n')
+        temp2 = temp.split(",")
+        a = int(temp2[0])
+        b = int(temp2[1])
+        alphas[a, b] = float(temp2[2])
+    
+    max_alpha = np.max(alphas)
+    #alphas are affinity measures - take -ve to get a dissimilarity measure
+    weights = max_alpha - alphas
+    w = np.asmatrix(weights)
+    G = nx.DiGraph(w)
+    return G 
 
+def main():
+    #pick 10 graphs at random
+
+    #pick 10 samples at random
+
+    p = float(sys.argv[1])
+
+    f = open('log_p_' + str(p) + '.txt', 'w')
+
+    N_list = [6, 7, 8, 9, 10]
+    niter_list = [10, 100, 1000]
+    data_dir = '/home/pankaj/Sampling/data/input/social_graphs/k_'
+    ngraphs = 4 
+    nsamples = 5 
+
+    for N in N_list:
+        for iter_num in niter_list:
+            graph_dir = data_dir + str(N) + '/'
+            file_list = os.listdir(graph_dir)
+            for i in range(ngraphs):
+                G = read_graph(graph_dir + file_list[i])
+                for j in range(nsamples):
+                    sample = torch.rand(int(math.pow(2, N))) > 0.95
+                    for k in range(20):
+                        tic = time.clock()
+                        val = ic_model(G, sample, p, iter_num).item()
+                        toc = time.clock()
+                        to_write_list = [N, iter_num, i, j, k, val, toc - tic]
+                        print ' '.join(map(str, to_write_list)) + '\n'
+                        f.write(' '.join(map(str, to_write_list)) + '\n')
 if __name__ == '__main__':
     main()
