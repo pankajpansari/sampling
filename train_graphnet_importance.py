4dfd1d5f90e61a80d27be0856c65bfb083636733
pankajpansari <pankaj.pansari1@gmail.com>
2018-07-03 16:51:00 +0100

    Logging loss in file. Refactored code to have separate function for training.

--- train_graphnet_importance.py
+++ train_graphnet_importance.py
@@ -23,6 +23,8 @@ np.random.seed(123)
 torch.set_printoptions(precision = 4)
 torch.manual_seed(123)
 
+eps = 1e-6
+
 def reconstruction_loss(x, y):
     #Reconstruction loss - L2 difference between x and y 
     batch_size = x.size()[0]
@@ -35,13 +37,39 @@ def getProb(sample, pVec):
     temp = pVec * sample + (1 - pVec)*(1 - sample)
     return torch.prod(temp, 1)
 
-def entropy_mc(output, nsamples):
+def entropy(output)
     batch_size = output.size()[0]
     ent_sum = torch.zeros(batch_size) 
     for t in range(nsamples):
         sample = torch.bernoulli(output)
         ent_sum += -torch.log(getProb(sample, output))
     return ent_sum.sum()/(nsamples*batch_size)
+
+def obj1(sample): #sample has 1 if item selected, else 0
+    N = sample.size 
+#    assert(sample.shape[0] == N) 
+    this_set = [i for i in range(N) if sample[i] == 1] 
+#    a = [0, 1, 2, 3, 4]
+    a = [0] 
+
+    flag = 0
+    for t in a:
+        if sample[t] == 1:
+            flag = 1
+    if flag == 1:
+        return Variable(torch.FloatTensor([1]))
+    else:
+        return Variable(torch.FloatTensor([1e5]))
+
+    if set(this_set).issubset(set(a)):
+        return Variable(torch.FloatTensor([1e5]))
+    else:
+        return Variable(torch.FloatTensor([1]))
+
+    if set(this_set) < set(a):
+        return Variable(torch.FloatTensor([1e5]))
+    else:
+        return Variable(torch.FloatTensor([1]))
 
 def kl_loss_mc_x(x, y, nsamples, influ_obj):
     #Sampling from x distribution
@@ -65,10 +93,11 @@ def kl_loss_mc_x(x, y, nsamples, influ_obj):
                 print xP, yP
                 print x_t, y_t, sample
                 sys.exit()
-            term1 =  torch.log(yP)
-            term2 =  torch.log(torch.abs(val)*xP)
+            term1 =  torch.log(yP + eps)
+            term2 =  torch.log(torch.abs(val)*xP + eps)
             term3 = yP/xP
             obj_t += term3*(term1 - term2) 
+#            obj_t += (term1 - term2) 
         obj[t] = obj_t/nsamples
     return obj.mean()
 
@@ -94,8 +123,8 @@ def kl_loss_mc_y(x, y, nsamples, influ_obj):
                 print xP, yP
                 print x_t, y_t, sample
                 sys.exit()
-            term1 =  torch.log(yP)
-            term2 =  torch.log(torch.abs(val)*xP)
+            term1 =  torch.log(yP + eps)
+            term2 =  torch.log(torch.abs(val)*xP + eps)
             obj_t += term1 - term2
         obj[t] = obj_t/nsamples
     return obj.mean()
@@ -119,6 +148,8 @@ def kl_loss_mc_uniform(x, y, nsamples, influ_obj):
             #draw a sample/set from the uniform distribution
             sample = Variable(torch.bernoulli(torch.FloatTensor([0.5]*N)))
             val = torch.abs(influ_obj(sample.numpy()))
+            if val > 1e3:
+                print 'hit'
             xP = getProb(sample, x_t)
             yP = getProb(sample, y_t)   
             if yP.item() == 0:
@@ -160,59 +191,31 @@ def get_adjacency(data_list, nNodes, graph_dir):
 
     return adjacency
 
+def training_mc(x, adj, node_feat, net, lr1, lr2, n_epochs1, n_epochs2, mom, nsamples_mc, file_prefix, p, num_influ_iter):
 
-def main():
-
-    lr1 = float(sys.argv[1])
-    lr2 = float(sys.argv[2])
-    mom = float(sys.argv[3])
-    n_epochs1 = int(sys.argv[4])
-    n_epochs2 = int(sys.argv[5])
-    batch_size = int(sys.argv[6])
-    p = 0.5
-    num_influ_iter = 100
-
-    graph_dir = '/home/pankaj/Sampling/data/input/social_graphs/N_32/'
-    data_list = ['g_N_32_99.txt']
-
-    nNodes = 32 
-    k = 9 #has no effect as of now
-    net = MyNet(k)
-
-    x = Variable(torch.rand(batch_size, nNodes), requires_grad = True) 
-#    y = Variable(torch.rand(batch_size, nNodes), requires_grad = True) 
-    temp = get_adjacency(data_list, nNodes, graph_dir)
-
-    #In this case, repeat adj batch_size number of times
-    adj = temp.repeat(batch_size, 1, 1)
-
-    node_feat = get_node_feat(adj, nNodes)
-
-    for params in net.parameters():
-        params.requires_grad = True
-
+    f = open(file_prefix + '_training_log.txt', 'w')
 
     optimizer = optim.Adam(net.parameters(), lr=lr1)
-#    optimizer = optim.SGD(net.parameters(), lr=lr)
 
-#    for epoch in range(n_epochs1):
-    for epoch in range(0):
+    for epoch in range(n_epochs1):
+#    for epoch in range(100):
 
         #get minibatch
         optimizer.zero_grad()   # zero the gradient buffers
     
         y = net(x, adj, node_feat) 
- 
+
         train_loss = reconstruction_loss(x, y)
 
         print "Epoch: ", epoch, "       reconstruction loss = ", train_loss.item()
+        f.write(str(0) + " " +  str(epoch) +  " " + str(train_loss.item()) + "\n")
 
         train_loss.backward()
     
         optimizer.step()    # Does the update
 
-#    torch.save(net, '/home/pankaj/Sampling/data/working/02_07_2018_temp.net')
-    net = torch.load('/home/pankaj/Sampling/data/working/02_07_2018_temp.net')
+#    torch.save(net.state_dict(), file_prefix + '_net.dat')
+#    net.load_state_dict(torch.load(file_prefix + '_net.dat'))
 
     G = nx.DiGraph(adj[0].numpy())
 
@@ -229,14 +232,57 @@ def main():
         y = net(x, adj, node_feat) 
  
 #        train_loss = kl_loss_mc_uniform(x, y, 10, influ_obj)
-        train_loss2 = kl_loss_mc_y(x, y, 100, influ_obj)
+        train_loss2 = kl_loss_mc_y(x, y, nsamples_mc, influ_obj)
 
         print "Epoch: ", epoch, "       KL-based loss = ", train_loss2.item()
+        f.write(str(1) + " " +  str(epoch) +  " " + str(train_loss2.item()) + "\n")
 
         train_loss2.backward()
     
         optimizer.step()    # Does the update
     
+    torch.save(net.state_dict(), file_prefix + '_net.dat')
+    f.close()
+    y = net(x, adj, node_feat) 
+    return y
+
+def main():
+
+    lr1 = float(sys.argv[1])
+    lr2 = float(sys.argv[2])
+    mom = float(sys.argv[3])
+    n_epochs1 = int(sys.argv[4])
+    n_epochs2 = int(sys.argv[5])
+    batch_size = int(sys.argv[6])
+    p = 0.5
+    num_influ_iter = 10
+
+    graph_dir = '/home/pankaj/Sampling/data/input/social_graphs/N_32/'
+    wdir = '/home/pankaj/Sampling/data/working/03_07_2018/'
+    data_list = ['g_N_32_99.txt']
+
+    nNodes = 32 
+    k = 9
+
+    net = MyNet(k)
+
+    param_list = [lr1, lr2, mom, n_epochs1, n_epochs2, batch_size, p, num_influ_iter, nNodes, k]
+    file_prefix =  wdir + '/graph_' + '_'.join(str(x) for x in param_list)
+
+    x = Variable(torch.rand(batch_size, nNodes), requires_grad = True) 
+
+    temp = get_adjacency(data_list, nNodes, graph_dir)
+
+    #In this case, repeat adj batch_size number of times
+    adj = temp.repeat(batch_size, 1, 1)
+
+    node_feat = get_node_feat(adj, nNodes)
+
+    for params in net.parameters():
+        params.requires_grad = True
+
+    y = training_mc(x, adj, node_feat, net, lr1, lr2, n_epochs1, n_epochs2, mom, 100, file_prefix, p, num_influ_iter) 
+    print x[0], y[0]
     sys.exit()
  
 if __name__ == '__main__':
