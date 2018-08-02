00a9725082b3a4e519449388a75ffe3a753af109
pankajpansari <pankaj.pansari1@gmail.com>
2018-08-02 10:48:27 +0100

    Function to read FW iterates added

--- read_files.py
+++ read_files.py
@@ -3,6 +3,20 @@ import sys
 import torch
 from torch.autograd import Variable
 
+def read_iterates(filename, N, num_iterates):
+
+    f = open(filename, 'rU')
+    x_list = []
+    for t in range(num_iterates):
+        x = []
+        for i in range(N):
+            val = float(next(f).strip('\n'))
+            x.append(val)
+        x_list.append(torch.Tensor(x))
+        assert(next(f) == '\n')
+    f.close()
+    return x_list
+
 def get_sfo_optimum(filename, N):
 
     x_good = Variable(torch.Tensor([0]*N))
@@ -57,4 +71,10 @@ def read_graph(graph_file, N):
     
     return G
 
-
+if __name__ == '__main__':
+    N = 512
+    g_id =  0
+    k = 20
+    read_iterates('/home/pankaj/Sampling/data/input/social_graphs/N_' + str(N)
+            + '/iterates/g_N_' + str(N) + '_' + str(g_id) + '_' + str(k)
+            + '_100_10_0.4_100_0_1_0.txt', N, 2)
