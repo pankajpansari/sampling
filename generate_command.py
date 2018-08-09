086fb357bc1f3c251d64954aa732b85742ec9580
pankajpansari <pankaj.pansari1@gmail.com>
2018-08-09 12:56:16 +0100

    Command generation for variance in FW solution - corrected

--- generate_command.py
+++ generate_command.py
@@ -9,16 +9,27 @@ import sys
 def verify_variance_files():
     N = 512
     p = 0.4
-    num_fw_iter = 10 
-    nsamples_mlr_list = [1, 5, 10, 20, 50, 100] 
+    num_fw_iter = 20 
+    nsamples_mlr_list = [1, 5, 10, 20, 50] 
     num_influ_iter = 100
     k = 20 #cardinality constraint
     a_list = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1] 
 
-    dirN = "/home/pankaj/Sampling/data/input/social_graphs/N_512/var_study/"
-    var_file_list = os.listdir(dirN)
+    dirN = "/home/pankaj/Sampling/data/input/social_graphs/N_512/fw_opt/"
+    opt_file_list = os.listdir(dirN)
 
     if_herd = 0
+    for nsamples in [1, 5, 10, 20, 50]:
+        for g_id in range(10):
+            temp = 'g_N_' + str(N) + '_' + str(g_id) 
+            seed = 123 + g_id
+            opt_file = '_'.join(str(y) for y in [temp, k, nsamples, num_fw_iter, p, num_influ_iter, 0, 0, 0.0, seed]) + '.txt'
+            if opt_file not in opt_file_list:
+                print(opt_file)
+#            if os.stat(dirN + opt_file).st_size == 0:
+#                print(opt_file)
+
+    sys.exit()
     for nsamples in nsamples_mlr_list:
         for i in range(10):
             for a in a_list:
@@ -34,14 +45,15 @@ def verify_variance_files():
 
 def command_solution_variance():
 
-    command_file = 'jobsQueued.txt'
+    command_file = 'working/jobsQueued.txt'
 
     f = open(command_file, 'w')
 
     for nsamples in [1, 5, 10, 20, 50]:
-        for g_id in range(10):
-            seed = 123 + g_id
-            print('python generate_ground_truth.py 512 ' + str(g_id) + ' 20 ' + str(nsamples) + ' 20 0.4 100 0 0 0 ' + str(seed), file = f)
+        for g_id in range(5):
+            for t in range(5):
+                seed = 123 + t 
+                print('python generate_ground_truth.py 512 ' + str(g_id) + ' 20 ' + str(nsamples) + ' 20 0.4 100 0 0 0 ' + str(seed), file = f)
 
 def command_ground_truth():
     #N_list = [6, 7, 8, 9, 10] #in log terms (that is, graph has math.pow(2, N) nodes)
