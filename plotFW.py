2a5ae19d912559e87b31c845d33953c8cf0e6035
pankajpansari <pankaj.pansari1@gmail.com>
2018-08-10 11:34:32 +0100

    Function to plot solution variance added. Deleting command generation script (does not really need version control)

--- plotFW.py
+++ plotFW.py
@@ -86,6 +86,51 @@ def difference(a):
      plt.title('Plot of difference in submodular function values between optimum and best of 20 randomly drawn sets (averaged over 8 graphs of 32 nodes)')
      plt.savefig(sys.argv[2])
 
+def plot_iterates_hist():
+    filename = '/home/pankaj/Sampling/data/input/social_graphs/N_512/iterates/' + sys.argv[1]
+    a = np.loadtxt(filename)
+    for i in range(10):
+        plt.subplot(5, 2, i + 1)
+        temp = a[512*i:512*(i + 1)]
+        plt.hist(temp, bins = 100, range = (0, 1))
+        plt.title("iter = " + str(i))
+    plt.show()
+#    plt.savefig(sys.argv[2])
+
+def plot_solution_variance():
+
+    solution_dir = '/home/pankaj/Sampling/data/input/social_graphs/N_512/fw_opt/'
+    d = []
+    e = []
+    nsamples_list = [1, 5, 10]
+    for nsamples in nsamples_list:
+        a = []
+        c = []
+        for g_id in range(5):
+            b = []
+            for t in range(5):
+                seed = 123 + t 
+                opt_file = 'g_N_512_' + str(g_id) + '_20_' + str(nsamples) + '_20_0.4_100_0_0_0.0_' + str(seed) + '.txt'
+                f = open(solution_dir + opt_file, 'r')
+                opt = float(next(f))
+                f.close()
+                b.append(opt)
+            a.append(np.var(b))
+            c.append(np.mean(b))
+        d.append(np.mean(a))
+        e.append(np.mean(c))
+    print d, e
+    plt.subplot(2, 1, 1)
+    plt.plot(nsamples_list, d)
+    plt.xlabel('# samples for relaxation estimation')
+    plt.ylabel('Variance of 5 rounded solutions (averaged over 5 graphs)')
+    plt.subplot(2, 1, 2)
+    plt.plot(nsamples_list, e)
+    plt.xlabel('# samples for relaxation estimation')
+    plt.ylabel('Mean of 5 rounded solutions (averaged over 5 graphs)')
+#    plt.savefig('variance_solution.jpg')
+    plt.show()
+
 # Gather our code in a main() function
 def main():
      filename = '/home/pankaj/Sampling/data/input/social_graphs/N_512/fw_log/' + sys.argv[1]
@@ -98,17 +143,7 @@ def main():
 	val.append(float(a[1]))
      linePlotSave(val, save_filename)
 
-def plot_iterates_hist():
-    filename = '/home/pankaj/Sampling/data/input/social_graphs/N_512/iterates/' + sys.argv[1]
-    a = np.loadtxt(filename)
-    for i in range(10):
-        plt.subplot(5, 2, i + 1)
-        temp = a[512*i:512*(i + 1)]
-        plt.hist(temp, bins = 100, range = (0, 1))
-        plt.title("iter = " + str(i))
-    plt.show()
-#    plt.savefig(sys.argv[2])
-
 if __name__ == '__main__':
 #    main()
-    plot_iterates_hist()
+#    plot_iterates_hist()
+    plot_solution_variance()
